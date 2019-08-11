37cabd28eab3ed756742ffa8ae5bd2469ec9de96
Sidney Barrah <neothedeveloper231@gmail.com>
2019-08-11 13:43:37 +0100

    BUGFIX - removed hardcoded url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-
+const baseUrl = window.location.protocol + '//' + window.location.host + '/';
 const instance = axios.create({
-  baseURL: 'http://localhost:3000/'
+  baseURL: baseUrl
 });
 
 export default instance;
